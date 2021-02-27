35b97c8465062718ba10a1d8d28531fb47d0ed78
iandk <hi@ianklemm.de>
2021-02-27 19:37:40 +0100

    Add favicon

--- public/view/header.view.php
+++ public/view/header.view.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,24 @@
     <link href="app/css/tailwind.css" rel="stylesheet" type="text/css">
     <link href="app/css/app.css" rel="stylesheet" type="text/css">
     <link href="app/css/fontawesome.css" rel="stylesheet" type="text/css">
+    <!-- FAVICONS -->
+    <link rel="apple-touch-icon" sizes="57x57" href="app/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="app/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="app/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="app/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="app/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="app/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="app/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="app/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="app/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="app/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="app/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="app/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="app/favicon/favicon-16x16.png">
+    <link rel="manifest" href="app/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="app/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body>
