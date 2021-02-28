fe8d02393703e0102b5a5ae34dffe4c0c3cd9195
iandk <hi@ianklemm.de>
2021-02-28 14:29:19 +0100

    Rename delete modal, remove outline

--- public/view/component/deletehost.php
+++ public/view/component/deletehost.php
@@ -36,7 +36,7 @@ leave-to-class="opacity-0"
         -->
         <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
         <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
-            <button type="button" @click="modalOpen = !modalOpen" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button type="button" @click="modalOpen = !modalOpen" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none">
             <span class="sr-only">Close</span>
             <!-- Heroicon name: x -->
             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -63,10 +63,10 @@ leave-to-class="opacity-0"
             </div>
         </div>
         <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-            <button type="button" @click="deleteServer()" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+            <button type="button" @click="deleteServer()" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm">
                 Delete
             </button>
-            <button type="button" @click="modalOpen = !modalOpen" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm">
+            <button type="button" @click="modalOpen = !modalOpen" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none sm:mt-0 sm:w-auto sm:text-sm">
                 Cancel
             </button>
         </div>
