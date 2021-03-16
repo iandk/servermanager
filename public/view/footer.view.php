d0e2ba227f4b920e112e56a798797269e14d19a8
iandk <hi@ianklemm.de>
2021-03-16 12:38:53 +0100

    Avoid js caching

--- public/view/footer.view.php
+++ public/view/footer.view.php
@@ -2,5 +2,5 @@
 </body>
 <script src="app/js/vue.js"></script>
 <script src="app/js/axios.js"></script>
-<script src="app/js/app.js"></script>
+<script src="app/js/app.js?v=16032021"></script>
 </html>
